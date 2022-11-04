3fc64115c68fc264a149bb4075affb8e201b00a0
VISHAD PATEL <pat.vishad@gmail.com>
2022-11-04 08:54:01 +0530

    updated database password

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const dbURL = 'mongodb+srv://vishad79:jW5dOBRUjMImSMfn@cluster0.zo4joh8.mongodb.net/task-manager?retryWrites=true&w=majority';
+const dbURL = 'mongodb+srv://vishad79:E6ExVkjRUeLmM3Nu@cluster0.zo4joh8.mongodb.net/task-manager?retryWrites=true&w=majority';
 const connectDb = (url)=>{
 	return mongoose.connect(dbURL);
 }
